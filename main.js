b665f31afd80d242fd7ed5ca6e6bff230df71a63
Habiba Ayman <HabibaAyman2004@outlook.com>
2025-02-01 20:42:35 +0200

    feat(global): set project entry

---
+++ main.js
@@ -0,0 +1,14 @@
+const { crawl } = require('./src/crawl');
+
+function main() {
+  if (process.argv.length !== 3) {
+    console.error('Usage: node main.js <input>');
+    process.exit(1);
+  }
+  console.log(`Crawling: ${process.argv[2]}`);
+
+  const baseUrl = process.argv[2];
+  crawl(baseUrl);
+}
+
+main();
