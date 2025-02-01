b856662cdc7d65b30be403699ee26d467085d14a
Habiba Ayman <HabibaAyman2004@outlook.com>
2025-02-02 00:10:46 +0200

    feat(global): add default start url

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ const { crawl } = require('./src/crawl');
 const { printReport } = require('./src/report');
 
 async function main() {
-  if (process.argv.length !== 3) {
+  if (process.argv.length < 3) {
     console.error('Usage: node main.js <input>');
     process.exit(1);
   }
   console.log(`Crawling: ${process.argv[2]}`);
 
   const baseUrl = process.argv[2];
-  const pages = await crawl(baseUrl, baseUrl, {});
+  const startUrl = process.argv[3] || baseUrl;
+
+  const pages = await crawl(baseUrl, startUrl, {});
 
   printReport(pages);
 }
